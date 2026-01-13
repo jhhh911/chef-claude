cf00cb09bd9fbbe19e09c0ec23b8c8ab0b84871e
James Heiniger <james.heiniger911@gmail.com>
2026-01-13 16:04:43 +1030

    Added new section and using conditional rendering to render ingredients list.

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React from "react";
 
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState(["Chicken", "Oregano", "Tomatoes"]);
+  const [ingredients, setIngredients] = React.useState([]);
 
   const ingredientsListItems = ingredients.map(ingredient => {
     return <li key={ingredient}>{ingredient}</li>;
   });
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget)
-    const newIngredient = formData.get('ingredient')
-    setIngredients(prev => [...prev, newIngredient])
+  function addIngredient(formData) {
+    const newIngredient = formData.get("ingredient");
+    setIngredients(prev => [...prev, newIngredient]);
+  }
+
+  function renderIngredients() {
+    return (
+      <section>
+        <h2>Ingredients on hand:</h2>
+        <ul className="ingredients-list" aria-live="polite">
+          {ingredientsListItems}
+        </ul>
+        <div className="get-recipe-container">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button>Get a recipe</button>
+        </div>
+      </section>
+    );
   }
 
   return (
     <main>
-      <form onSubmit={handleSubmit} className="add-ingredient-form">
+      <form action={addIngredient} className="add-ingredient-form">
         <input
           type="text"
           aria-label="Add Ingredient"
@@ -25,7 +41,7 @@ export default function Main() {
         />
         <button>Add ingredient</button>
       </form>
-      <ul>{ingredientsListItems}</ul>
+      {ingredients.length > 0 ? renderIngredients() : null}
     </main>
   );
 }
